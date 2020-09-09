1dee9b5af0a33e4fb06a8f45a8476ecd574bf4ff
Cassandra Rousseau <70165552+CassandraRousseau@users.noreply.github.com>
2020-09-08 20:28:08 -0400

    Drawing experiments- creating a face

--- experiments/drawing-experiments/js/script.js
+++ experiments/drawing-experiments/js/script.js
@@ -1,41 +1,38 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Drawing Experiments
+Cassandra Rousseau
 
-Here is a description of this template p5 project.
+Experimenting with P5's color and drawing functions
+Currently draws a face
 **************************************************/
 
 // setup()
 //
-// Description of setup() goes here.
+// Draws a face on a canvas.
 function setup() {
 createCanvas(500, 500);
 
-background(66, 135, 245);
+//Set the background to mint
+background(191, 255, 199);
 
+//Draw a flesh-colored head
+fill(250, 200, 200);//the flesh color
+ellipse(250, 250, 200, 200);// the head
 
-ellipseMode(CORNER);
-noStroke();
-fill(127, 0, 200, 100);
-ellipse(250, 250, 100, 100);
+//Draw the eyes
+fill(0, 0, 255);
+ellipse(200, 250, 30, 30);
+ellipse(300, 250, 30, 30);
 
-fill(137, 0, 210, 100);
-ellipse(250, 250, 80, 80);
-
-fill(147, 0, 220, 100);
-ellipse(250, 250, 60, 60);
-
-fill(157, 0, 230, 100);
-ellipse(250, 250, 40, 40);
-
-fill(167, 0, 240, 100);
-ellipse(250, 250, 20, 20);
+//Draw the mouth
+strokeWeight(10);
+line(200, 300, 300, 300);
 
 }
 
 // draw()
 //
-// Description of draw() goes here.
+// Does nothing.
 function draw() {
 
 }
