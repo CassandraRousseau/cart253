da1dbe6180f8c64f8f59ea17d2caa99ea0b24c56
Cassandra Rousseau <70165552+CassandraRousseau@users.noreply.github.com>
2020-09-09 11:25:32 -0400

    Correcting/finishing my alien drawing for activity 02

--- activities/02-draw-an-alien/js/script.js
+++ activities/02-draw-an-alien/js/script.js
@@ -12,32 +12,32 @@ function setup() {
 
 //Set the backgroud in pink
 createCanvas(640, 480);
-background(255, 127, 127);
+background(255, 100, 150);
 
 //Draw th body
 noStroke();
 ellipseMode(CENTER);
-fill(127, 127, 127);
-ellipse(300, 300, 100, 100);
+fill(127);
+ellipse(320, 470, 250, 200);
 
 //Draw th head
-fill(100, 100, 100);
-ellipse(300, 250, 75, 150);
+fill(100);
+ellipse(320, 250, 175, 270);
 
 //Draw the eyes
-fill(0,0,0);
-ellipse(275, 225, 25, 10);
-ellipse(325, 225, 25, 10);
+fill(0);
+ellipse(280, 225, 50, 115);
+ellipse(360, 225, 50, 115);
 
 //Draw the nostrils
-circle(285, 275, 10);
-circle(315, 275, 10);
+circle(305, 300, 10);
+circle(335, 300, 10);
 
 //Draw the mouth
 stroke(255, 0, 0);
-strokeWeight(4);
+strokeWeight(5);
 rectMode(CENTER);
-rect(300, 300, 75, 10);
+rect(320, 345, 90, 10);
 
 }
 
