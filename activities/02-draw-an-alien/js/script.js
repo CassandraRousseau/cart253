a43ddcb80e21d0d299533bdec55de7903da8ef51
Cassandra Rousseau <70165552+CassandraRousseau@users.noreply.github.com>
2020-09-08 22:10:07 -0400

    Creating my alien for the 02 actvity

--- activities/02-draw-an-alien/js/script.js
+++ activities/02-draw-an-alien/js/script.js
@@ -1,14 +1,43 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Drawing an Alien Activity
+Cassandra Rousseau
 
-Here is a description of this template p5 project.
+Creation of an alien.
 **************************************************/
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
+
+//Set the backgroud in pink
+createCanvas(640, 480);
+background(255, 127, 127);
+
+//Draw th body
+noStroke();
+ellipseMode(CENTER);
+fill(127, 127, 127);
+ellipse(300, 300, 100, 100);
+
+//Draw th head
+fill(100, 100, 100);
+ellipse(300, 250, 75, 150);
+
+//Draw the eyes
+fill(0,0,0);
+ellipse(275, 225, 25, 10);
+ellipse(325, 225, 25, 10);
+
+//Draw the nostrils
+circle(285, 275, 10);
+circle(315, 275, 10);
+
+//Draw the mouth
+stroke(255, 0, 0);
+strokeWeight(4);
+rectMode(CENTER);
+rect(300, 300, 75, 10);
 
 }
 
