5a6904515ad3cdc74cfd43fed17a603d5df8e326
Cassandra Rousseau <70165552+CassandraRousseau@users.noreply.github.com>
2020-09-15 18:42:13 -0400

    Learned about debugging variables

--- experiments/variables-experiments/js/script.js
+++ experiments/variables-experiments/js/script.js
@@ -5,14 +5,10 @@ Pippin Barr
 Here is a description of this template p5 project.
 **************************************************/
 let bgShade = 0;
-
-let circle = {
-  x:0,
-  y:250,
-  size:200,
-  speed:2,
-};
-
+let circleX = 0;
+let circleY=250;
+let circleSize = 200;
+let circleSpeed=2;
 
 // setup()
 //
@@ -20,7 +16,6 @@ let circle = {
 function setup() {
 
 createCanvas(500, 500);
-;
 
 }
 
@@ -30,8 +25,11 @@ createCanvas(500, 500);
 function draw() {
   bgShade=bgShade +1;
   background(bgShade);
-circle.x+= circle.speed;
+circleX+= circleSpeed;
 
-ellipse(circle.x,circle.y, circle.size);
+ellipse(circleX,circleY, circleSize);
+
+console.log(`circleX: ${circleX}, circleY: ${circleY},circleSize: ${circleSize},circleSpeed: ${circleSpeed}`);
+
 
 }
