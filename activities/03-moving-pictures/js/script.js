f9fb2d019401da59ba11b3a4161ac0ba09584008
Cassandra Rousseau <70165552+CassandraRousseau@users.noreply.github.com>
2020-09-15 21:01:00 -0400

    Started 03 activity

--- activities/03-moving-pictures/js/script.js
+++ activities/03-moving-pictures/js/script.js
@@ -1,20 +1,47 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Activity 03: Moving Pictures
+Cassandra Rousseau
 
 Here is a description of this template p5 project.
 **************************************************/
+let bg = {
+  r:0,
+  g:0,
+  b:0,
+};
+let circle1 ={
+x:0,
+y:250,
+size:100,
+fill:255,
+alpha:200,};
+let circle2={
+x:500,
+y:250,
+size:50,
+fill:255,
+alpha:225,
 
+};
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-
+createCanvas(500,500);
+noStroke();
 }
 
 // draw()
 //
-// Description of draw() goes here.
+//
 function draw() {
+  //background
+bg(bg.r,bg.g,bg.b);
+background.red+=1;
+//circle1
+fill(circle1.fill);
+ellipse(circle1.x,circle1.y,circle1.size)
+
+
 
 }
