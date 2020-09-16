a6d390b9a5ceea3f321c6a0bbf117e4ae94c2a5c
Cassandra Rousseau <70165552+CassandraRousseau@users.noreply.github.com>
2020-09-15 20:00:13 -0400

    A3:Started the moving pictures activities; learned map and constrain

--- activities/03-moving-pictures/js/script.js
+++ activities/03-moving-pictures/js/script.js
@@ -4,21 +4,11 @@ Pippin Barr
 
 Here is a description of this template p5 project.
 **************************************************/
-let bgShade = 0;
-let circle = {
-x:0,
-y:250,
-size:100,
-speed:1,
-fill:255
-};
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-
-createCanvas(500, 500);
 
 }
 
@@ -26,12 +16,5 @@ createCanvas(500, 500);
 //
 // Description of draw() goes here.
 function draw() {
-  background(bgShade);
-circle.x+= circle.speed;
-circle.x=constrain(circle.x,0,width);
-
-circle.fill=map(circle.x, 0,width,0,255);
-fill(circle.fill);
-ellipse(circle.x,circle.y, circle.size);
 
 }
