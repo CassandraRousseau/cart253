c25460e8d83dc3602e24bf707d7f26a8773431e8
Cassandra Rousseau <70165552+CassandraRousseau@users.noreply.github.com>
2020-09-22 19:52:25 -0400

    A4:Setting properties

--- activities/04-dodging-covid-19/js/script.js
+++ activities/04-dodging-covid-19/js/script.js
@@ -1,20 +1,32 @@
 /**************************************************
-Template p5 project
-Pippin Barr
-
-Here is a description of this template p5 project.
+Activity 04: Dodging COVID-19
+Cassandra Rousseau
+COVID-19, represented by a red circle, will move from the left side of
+the canvas to the right at a random y position. Each time it reaches the
+right side, it will reset to the left at a random y position. The user will
+control their own circle with the mouse position. If the COVID-19 circle touches
+the user circle, everything stops! In the background we see random static for visual
+ flair and we don’t see the mouse cursor.
 **************************************************/
-
+let covid19={
+x:0,
+y:250,
+size:100,
+vx:0,
+vy:0,
+speed:5
+}
+let red={
+  r:255,
+  g:0,
+  b:0
+}
 // setup()
 //
 // Description of setup() goes here.
-function setup() {
-
-}
+function setup() {}
 
 // draw()
 //
 // Description of draw() goes here.
-function draw() {
-
-}
+function draw() {}
