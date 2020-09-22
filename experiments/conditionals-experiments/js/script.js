949b14943c694ed0dc4360e3b3c7433c8a0b2738
Cassandra Rousseau <70165552+CassandraRousseau@users.noreply.github.com>
2020-09-22 17:22:20 -0400

    Learn loops

--- experiments/conditionals-experiments/js/script.js
+++ experiments/conditionals-experiments/js/script.js
@@ -3,26 +3,30 @@ Template p5 project
 Cassandra Rousseau
 Conditionals experiments
 **************************************************/
-let displayCircle= false;
+let caterpillar = {
+  x: 100,
+  y: 250,
+  segementSize: 50,
+};
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(500,500);
 }
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
   background(0);
-  if (mouseIsPressed){
-    displayCircle=true;
+  noStroke();
+  fill(100,200,100);
+  let x = caterpillar.x;
+  let numSegments = 6;
+  let segmentsDrawn = 0;
+  while (segmentsDrawn < numSegments) {
+    ellipse(x, caterpillar.y, caterpillar.segmentSize);
+    x = x + 40;
+    segmentsDrawn = segmentsDrawn + 1;
   }
-if(displayCircle){
-  ellipse(250,250,100,100);
-}
-
-
-
-
 }
