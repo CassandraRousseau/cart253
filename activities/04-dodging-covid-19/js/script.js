9b7fdb7619905fcfad7d6d33c45e023f6e82a8ac
Cassandra Rousseau <70165552+CassandraRousseau@users.noreply.github.com>
2020-09-23 10:13:06 -0400

    A4:Created the simulation

--- activities/04-dodging-covid-19/js/script.js
+++ activities/04-dodging-covid-19/js/script.js
@@ -21,21 +21,51 @@ fill:{
   b:0
 }
 }
-
+let user = {
+  x:250,
+  y:250,
+  size:100,
+  fill:255,
+}
+let numStatic= 500;
 // setup()
 //
 // Creating the canvas
 function setup() {
   createCanvas(windowWidth,windowHeight);
+  covid19.y=random(0,height);
+  covid19.vx=covid19.speed;
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
-  background(0);
+background(0);
+for (let i=0;i<numStatic;i++){
+  let x= random(0,width);
+  let y= random(0,height);
+  stroke(255);
+  point(x,y);
+}
+  noStroke();
+covid19.x+=covid19.vx;
+covid19.y+=covid19.vy;
+if (covid19.x>width){
+covid19.x=0;
+covid19.y=random(0,height);
+}
+
+user.x=mouseX;
+user.y=mouseY;
+let d= dist(user.x,user.y,covid19.x,covid19.y);
+  if (d<covid19.size/2+user.size/2){
+    noLoop();
+  }
 
   fill(covid19.fill.r,covid19.fill.g,covid19.fill.b);
-  noStroke();
   ellipse(covid19.x,covid19.y,covid19.size);
+  fill(user.fill)
+  ellipse(user.x,user.y,user.size)
+
 }
