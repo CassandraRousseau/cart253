9b63c9565d4a6fa5285eedb5aa34ec7164d795fa
Cassandra Rousseau <70165552+CassandraRousseau@users.noreply.github.com>
2020-09-27 15:17:34 -0400

    E2: Uploading images

--- exercises/exercise2/js/script.js
+++ exercises/exercise2/js/script.js
@@ -16,32 +16,30 @@ let covid19 = {
   vy: 0,
   speed: 5,
 };
-let user = {
+let mask = {
   x: 250,
   y: 250,
   w: 100,
   h: 50,
-  fill: {
+  tint: {
     r: 100,
     g: 100,
     b: 200,
-  }
-};
-let circle={
-  x:250,
-  y:250,
-  size:50,
-  fill:255
+  },
 };
 let numStatic = 300;
 // setup()
+function preload() {
+  covid19 = loadimage("assets/images/covidicon.png");
+  mask = loadimage("assets/images/bluemask.png");
+}
 //
 // Creating the canvas
 function setup() {
   createCanvas(windowWidth, windowHeight);
   covid19.y = random(0, height);
   covid19.vx = covid19.speed;
-noCursor();
+  noCursor();
 }
 
 // draw()
@@ -50,16 +48,14 @@ noCursor();
 function draw() {
   //Creating background
   background(255);
-
   //Display static
   for (let i = 0; i < numStatic; i++) {
     let x = random(0, width);
     let y = random(0, height);
     fill(255);
     stroke(200, 100, 100);
-    ellipse(x, y,50);
+    ellipse(x, y, 50);
   }
-
   //Creating COVID19 movement
   noStroke();
   covid19.x += covid19.vx;
@@ -69,31 +65,30 @@ function draw() {
     covid19.y = random(0, height);
   }
   if (covid19.y < height / 2) {
-    fill(255);
+    tint(255);
   } else {
-    fill(200, 100, 100);
+    tint(200, 100, 100);
   }
-  ellipse(covid19.x, covid19.y, covid19.size);
-
-  fill(user.fill.r, user.fill.g, user.fill.b);
-  rect(user.x, user.y, user.w, user.h);
+  image(covid19, covid19.x, covid.y, covid19.size);
+  tint(mask.tint.r, mask.tint.g, mask.tint.b);
+  rect(mask.x, mask.y, mask.w, mask.h);
 }
 //Creating catching effect
 function mouseDragged() {
-  user.x = mouseX;
-  user.y = mouseY;
-  let d = dist(user.x, user.y, covid19.x, covid19.y);
-  if (d < covid19.size / 2 + user.h/2) {
+  mask.x = mouseX;
+  mask.y = mouseY;
+  let d = dist(mask.x, mask.y, covid19.x, covid19.y);
+  if (d < covid19.size / 2 + mask.h / 2) {
     noLoop();
   }
   //Modifying user fill
-  if (user.x > width / 2) {
-    user.fill.r = 255;
-    user.fill.g = 255;
-    user.fill.b = 255;
+  if (mask.x > width / 2) {
+    mask.fill.r = 255;
+    mask.fill.g = 255;
+    mask.fill.b = 255;
   } else {
-    user.fill.r = 100;
-    user.fill.g = 100;
-    user.fill.b = 200;
+    mask.fill.r = 100;
+    mask.fill.g = 100;
+    mask.fill.b = 200;
   }
 }
