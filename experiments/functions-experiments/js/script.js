397292eea7fa30f5d191e91fe622dc06e7545036
Cassandra Rousseau <70165552+CassandraRousseau@users.noreply.github.com>
2020-09-29 16:58:59 -0400

    Learned automated movement

--- experiments/functions-experiments/js/script.js
+++ experiments/functions-experiments/js/script.js
@@ -4,7 +4,14 @@ Cassandra Rousseau
 
 Here is a description of this template p5 project.
 **************************************************/
-let bg = 0;
+let circle = {
+  x: 250,
+  y: 250,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 2,
+};
 // setup()
 //
 // Description of setup() goes here.
@@ -16,9 +23,20 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
-  background(bg);
-  if (keyIsDown(65)) {
-    rectMode(CENTER);
-    rect(250, 250, 100, 100);
+  background(0);
+  let dx = circle.x - mouseX;
+  let dy = circle.y - mouseY;
+  if (dx < 0) {
+    circle.vx = -circle.speed;
+  } else if (dx > 0) {
+    circle.vx = circle.speed;
   }
+  if (dy < 0) {
+    circle.vy = -circle.speed;
+  } else if (dy > 0) {
+    circle.vy = circle.speed;
+  }
+  circle.x += circle.vx;
+  circle.y += circle.vy;
+  ellipse(circle.x, circle.y, circle.size);
 }
