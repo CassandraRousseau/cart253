aec85011a2e64b238fac4dd9d631e72ce0a42e00
Cassandra Rousseau <70165552+CassandraRousseau@users.noreply.github.com>
2020-09-29 11:02:43 -0400

    Learned functions

--- experiments/functions-experiments/js/script.js
+++ experiments/functions-experiments/js/script.js
@@ -23,13 +23,30 @@ createCanvas(500,500);
 // Description of draw() goes here.
 function draw() {
 background(0);
-circle.x+=circle.vx;
-circle.y+=circle.vy;
-if (circle.x>width){
+move();
+wrap();
+display();
+}
+function move(){
+  circle.x+=circle.vx;
+  circle.y+=circle.vy;
+}
+function wrap(){
+  if (circle.x>width){
+  reset();
+  }
+
+}
+function display(){
+  fill(255,0,0);
+ellispe(circle.x,circle.y,circle.size);
+}
+function reset(){
   circle.x=0;
   circle.vx+=2;
+  circle.vy+= -0.25;
   circle.size+=5;
 }
-fill(255,0,0);
-ellipse(circle.x,circle.y,circle.size);
+function mousePressed(){
+reset();
 }
