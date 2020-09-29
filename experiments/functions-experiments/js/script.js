3465f73be14713558c26d0674305a3dee6732e9f
Cassandra Rousseau <70165552+CassandraRousseau@users.noreply.github.com>
2020-09-29 15:47:58 -0400

    Learned states

--- experiments/functions-experiments/js/script.js
+++ experiments/functions-experiments/js/script.js
@@ -4,37 +4,60 @@ Cassandra Rousseau
 
 Here is a description of this template p5 project.
 **************************************************/
-let circle= {
-  x:0,
-  y:250,
-  vx:0,
-  vy:0,
-  speed:2,
-  size:100,
-}
+let circle = {
+  x: 0,
+  y: 250,
+  vx: 0,
+  vy: 0,
+  speed: 2,
+  size: 100,
+};
+let state = "title";
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-createCanvas(500,500);
-circle.vx=circle.speed;
-textSize(32);
-textAlign(CENTER,CENTER);
+  createCanvas(500, 500);
+  circle.vx = circle.speed;
+  textSize(32);
+  textAlign(CENTER, CENTER);
 }
-let state='title';
+
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
-background(0);
-//text
-fill(255);
-text('Life',width/2,height/2);
-//Animation
-circle.x+=circle.vx;
-circle.y+=circle.vy;
-ellipse(circle.x,circle.y,circle.size);
-//Ending
-fill(127);
+  background(0);
+  if (state === "title") {
+    title();
+  } else if (state === "animation") {
+    animation();
+  } else if (state === "ending") {
+    ending();
+  }
+}
+function title() {
+  //text
+  fill(255);
+  text("Life", width / 2, height / 2);
+}
+function animation() {
+  //Animation
+  circle.x += circle.vx;
+  circle.y += circle.vy;
+  if (circle.x > width) {
+    state = "ending";
+  }
+  ellipse(circle.x, circle.y, circle.size);
+}
+function ending() {
+  //Ending
+  fill(127);
+  text("It is all over", width / 2, height / 2);
+}
 
+function keyPressed() {
+  if (state === "title") {
+    state = "animation";
+  }
 }
