ab41003b96001a329f8ca240515d9b060940f7ae
Cassandra Rousseau <70165552+CassandraRousseau@users.noreply.github.com>
2020-09-29 11:38:26 -0400

    Learned functions with parameters

--- experiments/functions-experiments/js/script.js
+++ experiments/functions-experiments/js/script.js
@@ -1,16 +1,10 @@
 /**************************************************
 Template p5 project
-Pippin Barr
+Cassandra Rousseau
 
 Here is a description of this template p5 project.
 **************************************************/
-let circle ={
-  x:0,
-  y:250,
-  size:100,
-  vx:1,
-  vy:0,
-}
+
 // setup()
 //
 // Description of setup() goes here.
@@ -23,30 +17,19 @@ createCanvas(500,500);
 // Description of draw() goes here.
 function draw() {
 background(0);
-move();
-wrap();
-display();
+parallels(100,100,5,1,100,1);
+parallels(50,50,10,2,20,10);
+parallels(200,200,15,7,3,20);
+parallels(312,257,20,5,300,1);
 }
-function move(){
-  circle.x+=circle.vx;
-  circle.y+=circle.vy;
-}
-function wrap(){
-  if (circle.x>width){
-  reset();
-  }
+function parallels(x,y,numLines,lineWidth,lineHeight,lineSpacing){
 
+  for(let i=0;i<numLines;i++){
+    noStroke();
+    fill(255);
+    rectMode(CENTER);
+    rect(x,y,lineWidth,lineHeight);
+    x+=lineSpacing;
 }
-function display(){
-  fill(255,0,0);
-ellispe(circle.x,circle.y,circle.size);
-}
-function reset(){
-  circle.x=0;
-  circle.vx+=2;
-  circle.vy+= -0.25;
-  circle.size+=5;
-}
-function mousePressed(){
-reset();
+
 }
