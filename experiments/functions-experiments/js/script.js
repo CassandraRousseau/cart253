5536755b32657009c02acaadab59a01525b62488
Cassandra Rousseau <70165552+CassandraRousseau@users.noreply.github.com>
2020-09-29 13:09:22 -0400

    Learned text

--- experiments/functions-experiments/js/script.js
+++ experiments/functions-experiments/js/script.js
@@ -4,16 +4,20 @@ Cassandra Rousseau
 
 Here is a description of this template p5 project.
 **************************************************/
-
+let hello= {
+  string:'Hello,world!',
+  x:250,
+  y:250,
+  vx:5,
+  vy:1,
+  size:64,
+}
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
 createCanvas(500,500);
-let hotCelsius = toCelsius(100);
-console.log('100 degrees Farenheit is ${hotCelsius} degrees Celsius.');
-let coldCelsius= toCelsius(10);
-console.log('10 degrees Farenheit is ${coldCelsius} degrees Celsius.');
+
 }
 
 // draw()
@@ -21,8 +25,14 @@ console.log('10 degrees Farenheit is ${coldCelsius} degrees Celsius.');
 // Description of draw() goes here.
 function draw() {
 background(0);
-}
-function toCelsius(farenheit){
-  let celsius =(farenheit-32)*5/9;
-  return celsius;
+hello.x+=hello.vx;
+hello.y+=hello.vy;
+hello.size+=1;
+textAlign(CENTER,CENTER);
+textSize(hello.size);
+textStyle(BOLD);
+fill(200,50,200);
+stroke(50,200,50);
+strokeWeight(3);
+text(hello.string,hello.x,hello.y);
 }
