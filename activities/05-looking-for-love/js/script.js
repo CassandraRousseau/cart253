c8b884c9a0d747ef8f9cf0d9f6d52b1beed9b520
Cassandra Rousseau <70165552+CassandraRousseau@users.noreply.github.com>
2020-09-29 18:37:20 -0400

    A5:Added comments

--- activities/05-looking-for-love/js/script.js
+++ activities/05-looking-for-love/js/script.js
@@ -23,29 +23,37 @@ let circle2 = {
 };
 // setup()
 //
-// Description of setup() goes here.
+// Creating the canvas.
 function setup() {
   createCanvas(500, 500);
   circle1.x = width / 3;
   circle2.x = (2 * width) / 3;
+  circle1.vx = random(-circle1.speed, circle1.speed);
+  circle1.vy = random(-circle1.speed, circle1.speed);
+  circle2.vx = random(-circle2.speed, circle2.speed);
+  circle2.vy = random(-circle2.speed, circle2.speed);
 }
 
 // draw()
 //
-// Description of draw() goes here.
+// Creating the animation.
 function draw() {
+  //Creating the background.
   background(0);
+  //Setting circles movements
   circle1.x += circle1.vx;
   circle1.y += circle1.vy;
-  circle1.vx = random(-circle1.speed, circle1.speed);
-  circle1.vy = random(-circle1.speed, circle1.speed);
-  ellipse(circle1.x, circle1.y, circle1.size);
   circle2.x += circle2.vx;
   circle2.y += circle2.vy;
-  circle2.vx = random(-circle2.speed, circle2.speed);
-  circle2.vy = random(-circle2.speed, circle2.speed);
+  //Display circles
+  ellipse(circle1.x, circle1.y, circle1.size);
   ellipse(circle2.x, circle2.y, circle2.size);
+  //Checking if circles go outside the canvas.
   if (circle.x < 0 || (circle.x > width && circle.y < 0) || circle.y > height) {
     string("Sad ending");
+  }
+  //Checking if circles overlapped
+  if (dis) {
+    string("True love!");
   }
 }
