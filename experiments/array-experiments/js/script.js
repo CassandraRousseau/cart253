ade68819654b308c3cc568695c3199eb7815814f
Cassandra Rousseau <70165552+CassandraRousseau@users.noreply.github.com>
2020-10-15 14:39:01 -0400

    Learned more arrays

--- experiments/array-experiments/js/script.js
+++ experiments/array-experiments/js/script.js
@@ -5,62 +5,20 @@ Cassandra Rousseau
 Here is a description of this template p5 project.
 **************************************************/
 "use strict";
-let school = [];
-let schoolSize = 100;
-//Our fish
-let fish1;
-let fish2;
-let fish3;
-let fish4;
-// Description of setup() goes here.
-function setup() {
-  createCanvas(600, 600);
-  for (let i = 0; i < schoolSize; i++) {
-    let fish = createFish(random(0, width), random(0, height));
-    school.push(fish);
-  }
+let barkSFX;
+let rates = [1.5, 1.75, 2, 2.5, 2.75, 3];
+function preload() {
+  barkSFX = loadSound("assets/sounds/bark.wav");
 }
 
-function createFish(x, y) {
-  let fish = {
-    x: x,
-    y: y,
-    size: 50,
-    vx: 0,
-    vy: 0,
-    speed: 2,
-  };
-  return fish;
+function setup() {
+  createCanvas(600, 600);
 }
-// draw()
-//
-// Description of draw() goes here.
 function draw() {
   background(0);
-  for (let i = 0; i < school.length; i++) {
-    moveFish(school[i]);
-    displayFish(school[i]);
-  }
-}
-function moveFish(fish) {
-  let change = random(0, 1);
-  if (change < 0.05) {
-    fish.vx = random(-fish.speed, fish.speed);
-    fish.vy = random(-fish.speed, fish.speed);
-  }
-  fish.x += fish.vx;
-  fish.y += fish.vy;
-  fish.x = constrain(fish.x, 0, width);
-  fish.y = constrain(fish.y, 0, height);
-}
-function displayFish(fish) {
-  push();
-  fill(200, 100, 100);
-  noStroke();
-  ellipse(fish.x, fish.y, fish.size);
-  pop();
 }
 function mousePressed() {
-  let fish = createFish(mouseX, mouseY);
-  school.push(fish);
+  let randomRate = random(rates);
+  barkSFX.rate(randomRate);
+  barkSFX.play();
 }
