71fee895064605e236e70321a1b9225ea6ffcef5
Cassandra Rousseau <70165552+CassandraRousseau@users.noreply.github.com>
2020-10-18 15:49:14 -0400

    E4:Added images array

--- exercises/exercise4/js/script.js
+++ exercises/exercise4/js/script.js
@@ -271,3 +271,22 @@ function keyPressed() {
     }
   }
 }
+// An array to store our images
+let images = [];
+// A variable storing the number of images to load
+let numImages = 10;
+// A variable to store the image we want to display
+let displayImage;
+
+// preload() loads 10 images
+function preload() {
+  // Use a for loop to count from 0 up to 9
+  for (let i = 0; i < numImages; i++) {
+    // Load the image into a variable
+    // Note that we use i to specify the number in the filename!
+    // Note how nice this is with a template literal string
+    let loadedImage = loadImage(`assets/images/clown-${i}.png`);
+    // Add the loaded image to the images array
+    images.push(loadedImage);
+  }
+}
