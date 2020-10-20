31d32fa841935645c1a81d0efbfccd4641b2f320
Cassandra Rousseau <70165552+CassandraRousseau@users.noreply.github.com>
2020-10-20 17:10:39 -0400

    Learned object oriented programming

--- experiments/object-oriented-programming-experiments/js/script.js
+++ experiments/object-oriented-programming-experiments/js/script.js
@@ -1,20 +1,38 @@
 /**************************************************
 Template p5 project
-Pippin Barr
+Cassandra Rousseau
 
 Here is a description of this template p5 project.
 **************************************************/
-
+"use strict";
+let garden = {
+  flowers: [],
+  numFlowers: 20,
+  grassColor: {
+    r: 120,
+    g: 180,
+    b: 120,
+  },
+};
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-
+  createCanvas(600, 600);
+  for (let i = 0; i < garden.numFlowers; i++) {
+    let flower = new Flower();
+    garden.flowers.push(flower);
+  }
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
-
+  background(garden.grassColor.r, garden.grassColor.g, garden.grassColor.b);
+  for (let i = 0; i < garden.flowers.length; i++);
+  {
+    let flower = garden.flowers[i];
+    flower.display();
+  }
 }
