5be157439710f04b6387060ac3a82297b1dad54f
Cassandra Rousseau <70165552+CassandraRousseau@users.noreply.github.com>
2020-10-20 17:39:34 -0400

    Learned constructors with parameters

--- experiments/object-oriented-programming-experiments/js/script.js
+++ experiments/object-oriented-programming-experiments/js/script.js
@@ -7,7 +7,7 @@ Here is a description of this template p5 project.
 "use strict";
 let garden = {
   flowers: [],
-  numFlowers: 20,
+  numFlowers: 30,
   grassColor: {
     r: 120,
     g: 180,
@@ -20,7 +20,16 @@ let garden = {
 function setup() {
   createCanvas(600, 600);
   for (let i = 0; i < garden.numFlowers; i++) {
-    let flower = new Flower();
+    let x = random(0, width);
+    let y = random(0, height);
+    let size = random(50, 80);
+    let stemLength = random(50, 100);
+    let petalColor = {
+      r: random(100, 255),
+      g: random(100, 255),
+      b: random(100, 255),
+    };
+    let flower = new Flower(x, y, size, stemLength, petalColor);
     garden.flowers.push(flower);
   }
 }
