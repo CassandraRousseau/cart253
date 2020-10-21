d63eef99ddbb7f9e8f2a2093a14cea37d7324211
Cassandra Rousseau <70165552+CassandraRousseau@users.noreply.github.com>
2020-10-21 11:06:58 -0400

    A7:Started object-oriented programming activity

---
+++ activities/07-Object-oriented-programming-activity/js/script.js
@@ -0,0 +1,18 @@
+/**************************************************
+Activity 07: Object-Oriented Programming
+Cassandra Rousseau
+The user will control a rectangular “paddle” at the bottom of the screen,
+ moving it horizontally with the mouse position. Balls will fall from the top of the screen.
+ If they hit the paddle, they will bounce upward and fall again. If they miss the paddle,
+ they will disappear off the bottom.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {}
