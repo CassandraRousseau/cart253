f45889579c4fe8f2c69b1581bca4d1ddbc3a0004
Cassandra Rousseau <70165552+CassandraRousseau@users.noreply.github.com>
2020-10-27 15:49:55 -0400

    A8:Started inheritance activity

---
+++ activities/08-inheritance-activity/js/script.js
@@ -0,0 +1,19 @@
+/**************************************************
+Activity 08:Inheritance
+Cassandra Rousseau
+
+The user will control a circular pedestrian at the bottom of the canvas.
+Cars, trucks, and motorcycles will be moving left and right across the canvas.
+If the pedestrian collides with any of the vehicles, they die.
+If they make it to the other side, they succeeded. Just like life!
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {}
