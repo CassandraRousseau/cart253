d5fc0e079255013c0dfd15442c9c39ee0e18b541
Cassandra Rousseau <70165552+CassandraRousseau@users.noreply.github.com>
2020-10-27 15:31:36 -0400

    Learned chains of inheritance

--- experiments/inheritance-experiments/js/script.js
+++ experiments/inheritance-experiments/js/script.js
@@ -7,6 +7,7 @@ Here is a description of this template p5 project.
 "use strict";
 let vehicles = [];
 let numCars = 10;
+let numSportsCars = 3;
 let numMotorcycles = 10;
 // setup()
 //
@@ -24,6 +25,12 @@ function setup() {
     let y = random(0, height);
     let motorcycle = new Motorcycle(x, y);
     vehicles.push(motorcycle);
+  }
+  for (let i = 0; i < numSportsCars; i++) {
+    let x = random(0, width);
+    let y = random(0, height);
+    let sportscar = new SportsCar(x, y);
+    vehicles.push(sportscar);
   }
 }
 
