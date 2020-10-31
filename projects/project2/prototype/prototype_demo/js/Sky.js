b856c202969fec357d4c67a1a0cbc3ef02b65ee3
Cassandra Rousseau <70165552+CassandraRousseau@users.noreply.github.com>
2020-10-31 12:43:08 -0400

    P2:Added comments

--- projects/project2/prototype/prototype_demo/js/Sky.js
+++ projects/project2/prototype/prototype_demo/js/Sky.js
@@ -1,17 +1,17 @@
 class Sky extends Level1 {
-  //Creating good ending screen
+  //Creating sky background for level 1
   constructor(skyImage) {
     this.x = 0;
     this.y = 0;
     this.image = skyImage;
   }
 
-  //Preloading background image for good ending
+  //Preloading background image for level 1
   preload() {
     this.image.preload();
   }
 
-  //Displaying good ending screen
+  //Displaying sky background
   display() {
     push();
     image(skyImage, this.x, this.y);
