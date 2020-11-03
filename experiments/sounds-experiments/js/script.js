97d70f6d6c2683fcf0cdb1a6e15d85f19f4a2a5c
Cassandra Rousseau <70165552+CassandraRousseau@users.noreply.github.com>
2020-11-03 15:38:23 -0500

    Learned p5 sound library

--- experiments/sounds-experiments/js/script.js
+++ experiments/sounds-experiments/js/script.js
@@ -4,13 +4,24 @@ Cassandra Rousseau
 
 Here is a description of this template p5 project.
 **************************************************/
-
-// setup()
-//
-// Description of setup() goes here.
-function setup() {}
+"use strict";
+let barkSFX;
+function preload() {
+  barkSFX = loadSound("assets/sounds/bark.wav");
+}
+function setup() {
+  createCanvas(600, 600);
+  userStartAudio();
+}
 
 // draw()
 //
 // Description of draw() goes here.
-function draw() {}
+function draw() {
+  background(0);
+  let newRate = map(mouseX, 0, width, -3, 3);
+  barkSFX.rate(newRate);
+}
+function mousePressed() {
+  barkSFX.play();
+}
