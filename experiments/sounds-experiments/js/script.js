64f328f0e3e3ae269ff4c40a1f4a25b1ea5f5c76
Cassandra Rousseau <70165552+CassandraRousseau@users.noreply.github.com>
2020-11-03 16:41:40 -0500

    Learned p5.Oscillator

--- experiments/sounds-experiments/js/script.js
+++ experiments/sounds-experiments/js/script.js
@@ -5,13 +5,14 @@ Cassandra Rousseau
 Here is a description of this template p5 project.
 **************************************************/
 "use strict";
-let barkSFX;
-function preload() {
-  barkSFX = loadSound("assets/sounds/bark.wav");
-}
+let oscillator;
+let t = 0;
+function preload() {}
 function setup() {
   createCanvas(600, 600);
   userStartAudio();
+  oscillator = new p5.Oscillator(1, "sine");
+  oscillator.amp(0.1);
 }
 
 // draw()
@@ -19,9 +20,14 @@ function setup() {
 // Description of draw() goes here.
 function draw() {
   background(0);
-  let newRate = map(mouseX, 0, width, -3, 3);
-  barkSFX.rate(newRate);
+  let noiseValue = noise(t);
+  let newFreq = map(noiseValue, 0, 1, 20, 2000);
+  oscillator.freq(newFreq);
+  t = t + 0.05;
 }
 function mousePressed() {
-  barkSFX.play();
+  oscillator.start();
+}
+function mouseReleased() {
+  oscillator.stop();
 }
