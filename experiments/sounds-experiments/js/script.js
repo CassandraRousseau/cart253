2acfff492d787a1247530830805b2c5e8ed3cb91
Cassandra Rousseau <70165552+CassandraRousseau@users.noreply.github.com>
2020-11-03 14:53:48 -0500

    Started learning sounds

---
+++ experiments/sounds-experiments/js/script.js
@@ -0,0 +1,16 @@
+/**************************************************
+Sound experiments
+Cassandra Rousseau
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {}
