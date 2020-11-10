c1bbb40bb4d7eb8299cc9956778746c83bc91fac
Cassandra Rousseau <70165552+CassandraRousseau@users.noreply.github.com>
2020-11-10 12:52:46 -0500

    E6:Added active=true in nature superclass; removed x/y parameters in magicPetal constructor

--- exercises/exercise6/js/MagicPetal.js
+++ exercises/exercise6/js/MagicPetal.js
@@ -1,27 +1,25 @@
 class MagicPetal extends Nature {
   //Creating the magic petal
-  constructor(x, y, magicPetalImage, mic) {
-    super(x, y, magicPetalImage, mic);
+  constructor(magicPetalImage, mic) {
+    super(magicPetalImage, mic);
     this.x = width / 2;
     this.y = height / 2;
     this.image = magicPetalImage;
     this.mic = mic;
-    this.active = true;
   }
   //Setting the magic petal
   move() {
     super.move();
     let scream = mic.getLevel();
-    // Check if right side rock is  moving
+    // Check if magic petal is  moving
     if (scream > this.movingThreshold) {
       this.state = "running";
-      // Right side rock is moving
-      this.x += this.vx;
-      this.y += this.vy;
     }
     if (this.w === this.maxWidth && this.h === this.maxHeight) {
       this.vx = 0;
     }
+    this.x += this.vx;
+    this.y += this.vy;
   }
 
   //Displaying the magic petal
--- exercises/exercise6/js/Nature.js
+++ exercises/exercise6/js/Nature.js
@@ -1,6 +1,6 @@
 class Nature {
   //Creating nature elements
-  constructor(x, y, angle, mic, alpha, active) {
+  constructor(x, y, angle, mic, alpha) {
     this.x = x;
     this.y = y;
     this.vx = 0;
@@ -22,7 +22,7 @@ class Nature {
     this.maxAlpha = 255;
     this.image = undefined;
     this.mic = mic;
-    this.active = active;
+    this.active = true;
   }
 
   //Preloading images of nature elements
