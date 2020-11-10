064e86c601acda6ea688ffa7bc8d352ce462da4c
Cassandra Rousseau <70165552+CassandraRousseau@users.noreply.github.com>
2020-11-10 10:14:00 -0500

    E6:Added cave background script; copied/pasted sky.js code of P2 prototype

---
+++ exercises/exercise6/js/Cave.js
@@ -0,0 +1,22 @@
+class Cave {
+  //Creating sky background for level 1
+  constructor(w, h, skyImage) {
+    this.x = 0;
+    this.y = 0;
+    this.width = w;
+    this.height = h;
+    this.image = skyImage;
+  }
+
+  //Preloading background image for level 1
+  preload() {
+    this.image.preload();
+  }
+
+  //Displaying sky background
+  display() {
+    push();
+    image(skyImage, this.x, this.y, this.width, this.height);
+    pop();
+  }
+}
