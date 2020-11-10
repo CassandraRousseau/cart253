2b670c3c65d9a2ade714e3a7645037f7c547db92
Cassandra Rousseau <70165552+CassandraRousseau@users.noreply.github.com>
2020-11-10 10:22:30 -0500

    E6:Added cave background for level

--- exercises/exercise6/js/Cave.js
+++ exercises/exercise6/js/Cave.js
@@ -1,22 +1,39 @@
 class Cave {
   //Creating sky background for level 1
-  constructor(w, h, skyImage) {
+  constructor(w, h, caveImage) {
     this.x = 0;
     this.y = 0;
-    this.width = w;
-    this.height = h;
-    this.image = skyImage;
+    this.w = w;
+    this.h = h;
+    this.alpha = alpha;
+    this.minAlpha = 0;
+    this.maxAlpha = 255;
+    this.image = caveImage;
   }
 
   //Preloading background image for level 1
   preload() {
     this.image.preload();
   }
+  //Changing the opacity of thorn
+  transparency(nature) {
+    push();
+    if (nature.move()) {
+      this.alpha = map(
+        this.alpha,
+        this.w,
+        this.h,
+        this.minAlpha,
+        this.maxAlpha
+      );
+    }
 
+    pop();
+  }
   //Displaying sky background
   display() {
     push();
-    image(skyImage, this.x, this.y, this.width, this.height);
+    image(caveImage, this.x, this.y, this.w, this.h);
     pop();
   }
 }
