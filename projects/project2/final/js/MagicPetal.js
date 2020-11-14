42af74612c501334f4bbd0684f448ecba9272946
Cassandra Rousseau <70165552+CassandraRousseau@users.noreply.github.com>
2020-11-14 13:13:16 -0500

    P2:Edited transparency method(changed how opacity will changed when parts of magicPetal moves to the center)

--- projects/project2/final/js/MagicPetal.js
+++ projects/project2/final/js/MagicPetal.js
@@ -1,6 +1,6 @@
 class MagicPetal {
   //Creating nature elements
-  constructor(x, y, mic, alpha,movingSpeed) {
+  constructor(x, y, mic, alpha, movingSpeed) {
     this.x = x;
     this.y = y;
     this.w = 300;
@@ -8,6 +8,7 @@ class MagicPetal {
     this.vx = 0;
     this.vy = 0;
     this.movingSpeed = movingSpeed;
+    this.stop = 0;
     this.state = "still";
     this.alpha = alpha;
     this.minAlpha = 0;
@@ -25,22 +26,21 @@ class MagicPetal {
   //Moving the nature elements
   move() {
     push();
-        // Check if magic petal is  moving based on screams
-        if (screaming) {
-          this.state = "running";
-          this.vx += this.movingSpeed;
-          this.vy += this.movingSpeed;
-        } else {
-          this.state = "still";
-        }
+    // Check if magic petal is  moving based on screams
+    if (screaming) {
+      this.state = "running";
+      this.vx += this.movingSpeed;
+      this.vy += this.movingSpeed;
+    } else {
+      this.state = "still";
+    }
 
-        //Setting when magic petal stops moving
-        if (this.x === width / 2 && this.y === height / 2) {
-          this.vx = 0;
-          this.vy = 0;
-        }
-        pop();
-
+    //Setting when magic petal stops moving
+    if (this.x === width / 2 && this.y === height / 2) {
+      this.vx = this.stop;
+      this.vy = this.stop;
+    }
+    pop();
   }
 
   //Changing the opacity of nature elements based on screams
@@ -49,15 +49,15 @@ class MagicPetal {
     if (state === "running") {
       this.alpha = map(
         this.vx,
-        this.minHeight,
-        this.maxHeight,
+        this.x,
+        this.stop,
         this.minAlpha,
         this.maxAlpha
       );
       this.alpha = map(
         this.vy,
-        this.,
-        this.maxWidth,
+        this.y,
+        this.stop,
         this.minAlpha,
         this.maxAlpha
       );
